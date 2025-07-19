4bd1ec53affcc1e5b366c83468109cbee1669dad
RGBCube <git@rgbcu.be>
2025-07-20 00:28:20 +0300

    Revert "bat: do not set pager to bat"
    This reverts commit 522a21658c0c4a0878f7de402f6ab7ebd445370a.

--- modules/common/bat.nix
+++ modules/common/bat.nix
@@ -1,7 +1,14 @@
 { config, lib, pkgs, ... }: let
   inherit (lib) enabled;
 in {
-  environment.shellAliases.cat  = "bat";
+  environment.variables = {
+    MANPAGER = "bat --plain";
+    PAGER    = "bat --plain";
+  };
+  environment.shellAliases = {
+    cat  = "bat";
+    less = "bat --plain";
+  };
 
   home-manager.sharedModules = [{
     programs.bat = enabled {
