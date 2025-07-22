30d4b5e421fa4172ba74231d76b9bb0ed89c9bc6
RGBCube <git@rgbcu.be>
2025-07-22 03:16:57 +0300

    python: remove attrValues and set UV env vars

--- modules/common/python.nix
+++ modules/common/python.nix
@@ -1,10 +1,13 @@
-{ lib, pkgs, ... }: let
-  inherit (lib) attrValues;
+{ pkgs, ... }: let
+  package = pkgs.python314;
 in {
-  environment.systemPackages = attrValues {
-    inherit (pkgs)
-      python314
-      uv
-    ;
+  environmennt.variables = {
+    UV_PYTHON_PREFERENCE = "system";
+    UV_PYTHON = "${package}";
   };
+
+  environment.systemPackages = [
+    package
+    pkgs.uv
+  ];
 }
