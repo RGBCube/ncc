c9fba656448a0947c6ab99421692dc30833f81ae
RGBCube <git@rgbcu.be>
2025-07-28 03:27:28 +0300

    flake: use const more

--- flake.nix
+++ flake.nix
@@ -88,13 +88,13 @@
     inherit (builtins) readDir;
     inherit (nixpkgs.lib) attrsToList const groupBy listToAttrs mapAttrs nameValuePair;
 
-    lib' = nixpkgs.lib.extend (_: _: nix-darwin.lib);
+    lib' = nixpkgs.lib.extend (const <| const <| nix-darwin.lib);
     lib  = lib'.extend <| import ./lib inputs;
 
     hostsByType = readDir ./hosts
       |> mapAttrs (name: const <| import ./hosts/${name} lib)
       |> attrsToList
-      |> groupBy ({ name, value }:
+      |> groupBy ({ value, ... }:
         if value ? class && value.class == "nixos" then
           "nixosConfigurations"
         else
