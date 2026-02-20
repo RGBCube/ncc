6a1320998457edaa9c3d4789a3f092647d6bbdaa
RGBCube <git@rgbcu.be>
2026-02-20 05:03:09 +0300

    ripgrep: remove todo alias

--- modules/ripgrep.mod.nix
+++ modules/ripgrep.mod.nix
@@ -2,17 +2,9 @@
 {
   flake.homeModules.ripgrep =
     { lib, pkgs, ... }:
-    let
-      inherit (lib.meta) getExe;
-
-      package = pkgs.ripgrep;
-    in
     {
-      programs.nushell.aliases.todo = # sh
-        ''${getExe package} "todo|fixme" --colors match:fg:yellow --colors match:style:bold'';
-
       packages = [
-        package
+        pkgs.ripgrep
       ];
 
       xdg.config.files."ripgrep/ripgreprc".generator = self.lib.generators.toCliFlagList;
