01a5c3c8eecc4f117e5cbbf49275da9bcd7d872c
RGBCube <git@rgbcu.be>
2026-03-13 21:56:57 +0300

    metrics: fix

--- modules/metrics.mod.nix
+++ modules/metrics.mod.nix
@@ -1,19 +1,14 @@
 {
-  flake.nixosModules.metrics-exporter =
-    { config, lib, ... }:
-    let
-      inherit (lib.modules) mkIf;
-    in
-    {
-      services.prometheus.exporters.node = mkIf config.isServer {
-        enable = true;
-        enabledCollectors = [
-          "processes"
-          "systemd"
-        ];
-        listenAddress = "[::]";
-      };
+  flake.nixosModules.metrics-exporter = {
+    services.prometheus.exporters.node = {
+      enable = true;
+      enabledCollectors = [
+        "processes"
+        "systemd"
+      ];
+      listenAddress = "[::]";
     };
+  };
 
   # TODO: nixosModules.metrics-server (prometheus + grafana)
 }
