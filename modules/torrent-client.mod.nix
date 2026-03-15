fe28ab8e9759a11ee92db8121b55ab976e7c236f
RGBCube <git@rgbcu.be>
2026-03-15 23:09:44 +0300

    qbittorrent: add mime types

--- modules/torrent-client.mod.nix
+++ modules/torrent-client.mod.nix
@@ -1,9 +1,25 @@
 {
-  flake.homeModules.torrent-client = { pkgs, ... }: {
-    # TODO: xdg.mime
+  flake.homeModules.torrent-client =
+    {
+      lib,
+      osConfig,
+      pkgs,
+      ...
+    }:
+    let
+      inherit (lib.modules) mkIf;
+      inherit (lib.trivial) const flip;
+      inherit (lib.attrsets) genAttrs;
+    in
+    {
+      xdg.mime-apps.default-applications = mkIf osConfig.nixpkgs.hostPlatform.isLinux
+        <| flip genAttrs (const "org.qbittorrent.qBittorrent.desktop") [
+          "application/x-bittorrent"
+          "x-scheme-handler/magnet"
+        ];
 
-    packages = [
-      pkgs.qbittorrent
-    ];
-  };
+      packages = [
+        pkgs.qbittorrent
+      ];
+    };
 }
