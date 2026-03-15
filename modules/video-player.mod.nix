e2d79e022c16b3846707ced41fafc7da86843c68
RGBCube <git@rgbcu.be>
2026-03-15 23:11:06 +0300

    haruna: add mime types

--- modules/video-player.mod.nix
+++ modules/video-player.mod.nix
@@ -8,9 +8,34 @@
     }:
     let
       inherit (lib.lists) singleton;
+      inherit (lib.modules) mkIf;
+      inherit (lib.trivial) const flip;
+      inherit (lib.attrsets) genAttrs;
     in
     {
-      # TODO: xdg-mime
+      xdg.mime-apps.default-applications =
+        mkIf osConfig.nixpkgs.hostPlatform.isLinux
+        <| flip genAttrs (const "org.kde.haruna.desktop") [
+          "audio/aac"
+          "audio/ac3"
+          "audio/flac"
+          "audio/mp4"
+          "audio/mpeg"
+          "audio/ogg"
+          "audio/vnd.wave"
+          "audio/webm"
+          "audio/x-matroska"
+          "audio/x-mpegurl"
+          "video/mp2t"
+          "video/mp4"
+          "video/mpeg"
+          "video/ogg"
+          "video/quicktime"
+          "video/vnd.avi"
+          "video/webm"
+          "video/x-matroska"
+          "video/x-ms-wmv"
+        ];
 
       packages = singleton (
         if osConfig.nixpkgs.hostPlatform.isLinux then
