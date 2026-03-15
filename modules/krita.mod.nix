9b852ed470c7798d3bc821f96d400f57b96f50ae
RGBCube <git@rgbcu.be>
2026-03-15 23:09:30 +0300

    krita: add mime types

--- modules/krita.mod.nix
+++ modules/krita.mod.nix
@@ -17,8 +17,17 @@
     }:
     let
       inherit (lib.modules) mkIf;
+      inherit (lib.trivial) const flip;
+      inherit (lib.attrsets) genAttrs;
     in
     {
+      xdg.mime-apps.default-applications = mkIf osConfig.nixpkgs.hostPlatform.isLinux
+        <| flip genAttrs (const "org.kde.krita.desktop") [
+          "application/x-krita"
+          "image/openraster"
+          "application/x-krita-paintoppreset"
+        ];
+
       packages = mkIf osConfig.nixpkgs.hostPlatform.isLinux [
         pkgs.krita
       ];
