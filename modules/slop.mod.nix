8d72a9d45981d7d753fc50f8f41eb793a2e61794
RGBCube <git@rgbcu.be>
2026-03-18 15:45:57 +0300

    slop.claude-code: add --rebuild and pass in arguments

--- modules/slop.mod.nix
+++ modules/slop.mod.nix
@@ -745,71 +745,73 @@ in
         <| pkgs.writeScriptBin "claude" /* nu */ ''
           #!${getExe pkgs.nushell}
 
-          let cache_global = $env
-          | get --optional "XDG_CACHE_HOME"
-          | default ($env.HOME | path join ".cache")
+          def --wrapped main [--rebuild, ...arguments] {
+            let cache_global = $env
+            | get --optional "XDG_CACHE_HOME"
+            | default ($env.HOME | path join ".cache")
 
-          let cache = $cache_global | path join "claude-code"
+            let cache = $cache_global | path join "claude-code"
 
-          let version = do {
-            let version_file = $cache | path join "latest-version"
-            let ttl = 6hr
+            let version = do {
+              let version_file = $cache | path join "latest-version"
+              let ttl = 6hr
 
-            match (try { (date now) - (ls $version_file | get 0.modified) > $ttl }) {
-              # Version older than 6h or doesn't exist.
-              true | null => {
-                let version = try {
-                  http get --max-time 5sec https://registry.npmjs.org/@anthropic-ai/claude-code/latest | get version
+              match (try { (date now) - (ls $version_file | get 0.modified) > $ttl }) {
+                # Version older than 6h or doesn't exist.
+                true | null => {
+                  let version = try {
+                    http get --max-time 5sec https://registry.npmjs.org/@anthropic-ai/claude-code/latest | get version
+                  } catch {
+                    print --stderr $"(ansi yellow_bold)warn:(ansi reset) fetched version older than 6hr, but can't re-fetch"
+                    return ""
+                  }
+
+                  try {
+                    $version_file | path parse | get parent | mkdir $in
+                    $version | save --force $version_file
+                  } catch {
+                    print --stderr $"(ansi yellow_bold)warn:(ansi reset) failed to save latest fetched version"
+                  }
+
+                  $version
+                },
+
+                # Version fetched within 6h.
+                false => { try {
+                  open $version_file
                 } catch {
-                  print --stderr $"(ansi yellow_bold)warn:(ansi reset) fetched version older than 6hr, but can't re-fetch"
-                  return ""
-                }
+                  print --stderr $"(ansi yellow_bold)warn:(ansi reset) failed to read latest fetched version"
+                  ""
+                } },
+              }
+            }
 
-                try {
-                  $version_file | path parse | get parent | mkdir $in
-                  $version | save --force $version_file
-                } catch {
-                  print --stderr $"(ansi yellow_bold)warn:(ansi reset) failed to save latest fetched version"
-                }
+            let binary_path = if ($version | is-empty) {
+              print --stderr $"(ansi yellow_bold)warn:(ansi reset) falling back to latest binary"
 
-                $version
-              },
-
-              # Version fetched within 6h.
-              false => { try {
-                open $version_file
+              try {
+                glob ($cache)/claude-code-* | last
               } catch {
-                print --stderr $"(ansi yellow_bold)warn:(ansi reset) failed to read latest fetched version"
-                ""
-              } },
+                print --stderr $"(ansi red_bold)error:(ansi reset) no binary found"
+                exit 67
+              }
+            } else {
+              $cache | path join $"claude-code-($version)"
             }
-          }
 
-          let binary_path = if ($version | is-empty) {
-            print --stderr $"(ansi yellow_bold)warn:(ansi reset) falling back to latest binary"
-
-            try {
-              glob ($cache)/claude-code-* | last
-            } catch {
-              print --stderr $"(ansi red_bold)error:(ansi reset) no binary found"
-              exit 67
+            if not ($binary_path | path exists) or $rebuild {
+              ${getExe pkgs.deno} cache $"npm:@anthropic-ai/claude-code@($version)"
+              ${getExe patch} ($cache_global | path join "deno" "npm" "registry.npmjs.org" "@anthropic-ai" "claude-code" $version "cli.js")
+              ${getExe pkgs.deno} compile --allow-all --output $binary_path $"npm:@anthropic-ai/claude-code@($version)"
             }
-          } else {
-            $cache | path join $"claude-code-($version)"
+
+            $env.PATH ++= [ "${pkgs.ripgrep}/bin" ]
+            r#'${
+              lib.strings.toJSON config.xdg.config.files."claude-code/settings.json".value.env
+            }'# | from json | load-env
+
+            exec $binary_path ...$arguments
           }
-
-          if not ($binary_path | path exists) {
-            ${getExe pkgs.deno} cache $"npm:@anthropic-ai/claude-code@($version)"
-            ${getExe patch} ($cache_global | path join "deno" "npm" "registry.npmjs.org" "@anthropic-ai" "claude-code" $version "cli.js")
-            ${getExe pkgs.deno} compile --allow-all --output $binary_path $"npm:@anthropic-ai/claude-code@($version)"
-          }
-
-          $env.PATH ++= [ "${pkgs.ripgrep}/bin" ]
-          r#'${
-            lib.strings.toJSON config.xdg.config.files."claude-code/settings.json".value.env
-          }'# | from json | load-env
-
-          exec $binary_path
         '';
     };
 }
