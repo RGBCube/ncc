8fe1ac634a3b5e085ff4835dd172036a76f758ca
RGBCube <git@rgbcu.be>
2026-03-27 21:15:59 +0300

    stub-ld: nuke

--- modules/nuke-default-packages.mod.nix
+++ modules/nuke-default-packages.mod.nix
@@ -4,6 +4,16 @@ let
   };
 in
 {
-  flake.nixosModules.nuke-default-packages = commonModule;
+  flake.nixosModules.nuke-default-packages =
+    { lib, ... }:
+    let
+      inherit (lib.lists) singleton;
+    in
+    {
+      imports = singleton commonModule;
+
+      environment.stub-ld.enable = false;
+    };
+
   flake.darwinModules.nuke-default-packages = commonModule;
 }
