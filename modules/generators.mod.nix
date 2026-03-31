7aa1d17a7c82714ed799acb7569ae238f20bcc67
RGBCube <git@rgbcu.be>
2026-03-31 20:38:23 +0300

    generators: separate toCliFlagList and toCliArgumentList

--- modules/generators.mod.nix
+++ modules/generators.mod.nix
@@ -2,14 +2,34 @@
 let
   inherit (lib.attrsets) mapAttrsToList;
   inherit (lib.strings) concatLines;
+  inherit (lib.lists) singleton flatten;
 in
 {
-  # CLI flag config format used by bat, ripgrep, etc.
+  # CLI flag config format used by bat.
   # true -> --flag, string/int -> --flag=value
   flake.lib'.generators.toCliFlagList =
     attrs:
-    concatLines
-    <| mapAttrsToList (
+    attrs
+    |> mapAttrsToList (
       name: value: if value == true then "--${name}" else "--${name} '${toString value}'"
-    ) attrs;
+    )
+    |> concatLines;
+
+  # CLI flag config format used by ripgrep.
+  # true -> --flag, string/int -> --flag<newline>value
+  flake.lib'.generators.toCliArgumentList =
+    attrs:
+    attrs
+    |> mapAttrsToList (
+      name: value:
+      if value == true then
+        singleton "--${name}"
+      else
+        [
+          "--${name}"
+          (toString value)
+        ]
+    )
+    |> flatten
+    |> concatLines;
 }
--- modules/ripgrep.mod.nix
+++ modules/ripgrep.mod.nix
@@ -7,7 +7,7 @@
         pkgs.ripgrep
       ];
 
-      xdg.config.files."ripgrep/config".generator = self.lib.generators.toCliFlagList;
+      xdg.config.files."ripgrep/config".generator = self.lib.generators.toCliArgumentList;
       xdg.config.files."ripgrep/config".value = {
         line-number = true;
         smart-case = true;
