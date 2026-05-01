13cd8400cba7a04feb301604d2d2026b3982efda
RGBCube <git@rgbcu.be>
2026-05-01 23:18:40 +0300

    linux-boot: enable systemd-boot commandline editor

--- modules/linux-boot.mod.nix
+++ modules/linux-boot.mod.nix
@@ -1,17 +1,16 @@
 {
-  flake.nixosModules.boot =
-    {
-      boot.initrd.systemd.enable = true;
+  flake.nixosModules.boot = {
+    boot.initrd.systemd.enable = true;
 
-      boot.loader.systemd-boot = {
-        enable = true;
-        editor = false;
-      };
-
-      boot.loader.efi.canTouchEfiVariables = true;
-
-      boot.supportedFilesystems = {
-        bcachefs = true;
-      };
+    boot.loader.systemd-boot = {
+      enable = true;
+      # editor = false; # Security Theather. Hell, Security Cinema even.
     };
+
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    boot.supportedFilesystems = {
+      bcachefs = true;
+    };
+  };
 }
