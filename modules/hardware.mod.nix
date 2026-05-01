ea9508097abd6e37dde03bea4312474aa79e7f44
RGBCube <git@rgbcu.be>
2026-05-01 15:59:35 +0300

    hardware: slurp up sound and bluetooth

--- modules/hardware.mod.nix
+++ modules/hardware.mod.nix
@@ -12,4 +12,43 @@
         (mkAliasOptionModule [ "hardware" "report" ] [ "facter" "reportPath" ])
       ];
     };
+
+  flake.nixosModules.bluetooth-gui =
+    { config, lib, ... }:
+    let
+      inherit (lib.modules) mkIf;
+    in
+    {
+      services.blueman.enable = mkIf (config.hardware.report.hardware.bluetooth or [] != []) true;
+    };
+
+  # hardware.bluetooth.enable is set by nixos-facter.
+  flake.nixosModules.bluetooth =
+    { config, lib, ... }:
+    let
+      inherit (lib.modules) mkIf;
+    in
+    {
+      hardware.bluetooth.powerOnBoot = mkIf (config.hardware.report.hardware.bluetooth or [] != []) true;
+    };
+
+  flake.nixosModules.sound =
+    { config, lib, ... }:
+    let
+      inherit (lib.modules) mkIf;
+    in
+    {
+      config = mkIf (config.hardware.report.hardware.sound or [] != []) {
+        security.rtkit.enable = true;
+
+        services.pipewire = {
+          enable = true;
+
+          alsa.enable = true;
+          alsa.support32Bit = true;
+
+          pulse.enable = true;
+        };
+      };
+    };
 }
