0ace4748eaf2869ee6e3debbab0c284b1e584e92
RGBCube <git@rgbcu.be>
2026-05-03 01:12:15 +0300

    kernel: fix sysctl for CAP_BPF requirements

--- modules/linux-kernel.mod.nix
+++ modules/linux-kernel.mod.nix
@@ -41,7 +41,7 @@
         "kernel.perf_event_paranoid" = 3;
 
         # Require CAP_BPF to use bpf.
-        "kernel.unprvileged_bpf_disabled" = 1;
+        "kernel.unprivileged_bpf_disabled" = 1;
       };
 
       # https://www.kernel.org/doc/html/latest/admin-guide/kernel-parameters.html
