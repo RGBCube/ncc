ddd55aa9be90f7fcb6656cd00fb68c1e27f0d3dc
RGBCube <git@rgbcu.be>
2026-05-09 00:20:54 +0300

    boot: enable emergency shell access

--- modules/linux-boot.mod.nix
+++ modules/linux-boot.mod.nix
@@ -1,12 +1,20 @@
 {
-  flake.nixosModules.boot = {
-    boot.initrd.systemd.enable = true;
+  flake.nixosModules.boot =
+    { config, lib, ... }:
+    let
+      inherit (lib.modules) mkDefault;
+    in
+    {
+      boot.initrd.systemd = {
+        enable = true;
+        emergencyAccess = mkDefault config.users.users.root.hashedPasswordFile;
+      };
 
-    boot.loader.systemd-boot = {
-      enable = true;
-      # editor = false; # Security Theather. Hell, Security Cinema even.
+      boot.loader.systemd-boot = {
+        enable = true;
+        # editor = false; # Security Theather. Hell, Security Cinema even.
+      };
+
+      boot.loader.efi.canTouchEfiVariables = true;
     };
-
-    boot.loader.efi.canTouchEfiVariables = true;
-  };
 }
