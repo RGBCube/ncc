694aceaae15d32262136870a64ba9b03325e1541
RGBCube <git@rgbcu.be>
2026-05-21 21:49:13 +0300

    jujutsu: add fork alias

--- modules/version-control.mod.nix
+++ modules/version-control.mod.nix
@@ -129,6 +129,27 @@
 
         aliases.u = [ "undo" ];
 
+        aliases.fork = [
+          "util"
+          "exec"
+          "--"
+          (pkgs.writeScript "jj-fork" /* nu */ ''
+            #!${getExe pkgs.nushell}
+            #
+
+            let original = (jj git remote list | lines | parse "{name} {url}" | where name == "origin" | get 0.url)
+
+            gh repo fork
+            gh repo set-default $original
+
+            let trunk = (jj config get 'revset-aliases."trunk()"')
+            jj config set --repo 'revset-aliases."trunk()"' ($trunk | str replace "origin" "upstream")
+
+            jj git fetch
+            jj bookmark track ($trunk | split row "@" | first) --remote upstream
+          '')
+        ];
+
         revsets.bookmark-advance-to = # python
           ''
             heads(::@ & ~description(exact:"") & (~empty() | merges()))
