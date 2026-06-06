4ca066855f63d56526c153914a8408ba92d02c1c
RGBCube <git@rgbcu.be>
2026-06-06 16:11:26 +0300

    nushell: drop some aliases, system.build.setEnvironmentNu

--- modules/nushell/nushell.mod.nix
+++ modules/nushell/nushell.mod.nix
@@ -4,16 +4,38 @@
     {
       lib,
       pkgs,
+      config,
       ...
     }:
     let
       inherit (lib.meta) getExe;
       inherit (lib.trivial) const;
-      inherit (lib.attrsets) genAttrs;
+      inherit (lib.attrsets)
+        genAttrs
+        mapAttrs
+        mapAttrsToList
+        zipAttrsWith
+        ;
+      inherit (lib.lists)
+        concatLists
+        concatMap
+        toList
+        ;
+      inherit (lib.strings)
+        concatLines
+        concatStringsSep
+        replaceStrings
+        ;
     in
     {
       users.defaultUserShell = inputs.crash.packages.${pkgs.stdenv.hostPlatform.system}.default;
-      environment.sessionVariables.SHELLS = "${getExe pkgs.nushell}:${getExe pkgs.bash}";
+      environment.sessionVariables.SHELLS =
+        [
+          pkgs.nushell
+          pkgs.bash
+        ]
+        |> map getExe
+        |> concatStringsSep ":";
 
       environment.shells = map getExe [
         pkgs.nushell
@@ -21,6 +43,59 @@
       ];
 
       environment.shellAliases = genAttrs [ "ls" "ll" "l" ] (const null);
+
+      system.build.setEnvironmentNu =
+        let
+          absoluteVariables = config.environment.variables |> mapAttrs (const toList);
+
+          suffixedVariables =
+            config.environment.profileRelativeEnvVars
+            |> mapAttrs (
+              const (
+                suffixes:
+                config.environment.profiles |> concatMap (profile: map (suffix: "${profile}${suffix}") suffixes)
+              )
+            );
+
+          allVariables = zipAttrsWith (const concatLists) [
+            absoluteVariables
+            suffixedVariables
+          ];
+
+          assignments =
+            allVariables
+            |> mapAttrsToList (
+              name: segments: /* nu */ ''
+                $env.${name} = $"${
+                  segments
+                  |> concatStringsSep ":"
+                  |>
+                    replaceStrings
+                      [
+                        "\${XDG_STATE_HOME}"
+                        "\$HOME"
+                        "\${HOME}"
+                        "\$USER"
+                        "\${USER}"
+                      ]
+                      [
+                        "($env.XDG_STATE_HOME? | default ($env.HOME + '/.local/state'))"
+                        "($env.HOME)"
+                        "($env.HOME)"
+                        "($env.USER)"
+                        "($env.USER)"
+                      ]
+                }"
+              ''
+            )
+            |> concatLines;
+        in
+        pkgs.writeText "set-environment.nu" ''
+          if ($env.__NIXOS_SET_ENVIRONMENT? | is-not-empty) { } else {
+            $env.__NIXOS_SET_ENVIRONMENT = "1"
+            ${assignments}
+          }
+        '';
     };
 
   flake.homeModules.nushell =
@@ -32,69 +107,9 @@
       ...
     }:
     let
-      inherit (lib.attrsets)
-        attrNames
-        attrValues
-        concatMapAttrs
-        filterAttrs
-        mapAttrs
-        mapAttrsToList
-        ;
-      inherit (lib.lists)
-        filter
-        last
-        ;
       inherit (lib.meta) getExe;
-      inherit (lib.modules) mkIf mkAfter;
-      inherit (lib.trivial) const flip;
-      inherit (lib.strings)
-        concatStrings
-        readFile
-        replaceStrings
-        splitString
-        ;
-    in
-    let
-      variablesMap =
-        {
-          HOME = config.directory;
-          USER = config.directory |> splitString "/" |> filter (s: s != "") |> last;
-
-          XDG_CACHE_HOME = config.xdg.cache.directory;
-          XDG_CONFIG_HOME = config.xdg.config.directory;
-          XDG_DATA_HOME = config.xdg.data.directory;
-          XDG_STATE_HOME = config.xdg.state.directory;
-        }
-        |> concatMapAttrs (
-          name: value: {
-            "\$${name}" = value;
-            "\${${name}}" = value;
-          }
-        );
-
-      nuVariables =
-        osConfig.environment.variables
-        |> flip removeAttrs [
-          "XDG_CONFIG_DIRS"
-          "XDG_DATA_DIRS"
-          "EDITOR"
-          "PAGER"
-        ]
-        |> mapAttrs (const <| replaceStrings (attrNames variablesMap) (attrValues variablesMap))
-        |> filterAttrs (name: const <| name != "TERM");
-
-      nuVariables' =
-        nuVariables
-        |> mapAttrsToList (
-          name: value:
-          # nu
-          ''
-            $env.${name} = "${value}"
-          ''
-        )
-        |> concatStrings;
-
-      nuConfig = nuVariables' + readFile ./nushell.config.nu;
+      inherit (lib.modules) mkIf mkAfter mkBefore;
+      inherit (lib.strings) readFile;
     in
     {
       xdg.config.files."zsh/.zshrc" = mkIf osConfig.nixpkgs.hostPlatform.isDarwin {
@@ -105,10 +120,16 @@
           '';
       };
 
+      xdg.config.files."nushell/config.nu".text =
+        mkIf osConfig.nixpkgs.hostPlatform.isLinux
+        <| mkBefore /* nu */ ''
+          source ${osConfig.system.build.setEnvironmentNu}
+        '';
+
       programs.nushell = {
         enable = true;
 
-        extraConfig = nuConfig;
+        extraConfig = readFile ./nushell.config.nu;
 
         aliases = {
           e = "^$env.EDITOR";
@@ -116,10 +137,8 @@
           la = "ls --all";
           ll = "ls --long";
           lla = "ls --long --all";
-          sl = "ls";
 
           cp = "cp --recursive --verbose --progress";
-          mk = "mkdir";
           mv = "mv --verbose";
           rm = "rm --recursive --verbose";
 
