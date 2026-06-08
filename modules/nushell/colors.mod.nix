1e8641c72a7ffade51d65cae9be1fb16df948112
RGBCube <git@rgbcu.be>
2026-06-08 19:30:02 +0300

    nushell.colors: no more ifd

--- modules/nushell/colors.mod.nix
+++ modules/nushell/colors.mod.nix
@@ -12,21 +12,11 @@
     {
       packages = singleton pkgs.vivid;
 
-      extraDependencies = singleton lsColors;
-
       programs.nushell.settings = {
         color_config.row_index = "light_yellow_bold";
         color_config.header = "light_yellow_bold";
       };
 
-      # FIXME: Decomissioned until nh fixes their remote build
-      # (to build with --eval-store auto --store <ssh> rather
-      # than evaling drv then uploading it then building remotely)
-      #
-      # # Yes, IFD. Deal with it.
-      # environment.sessionVariables.LS_COLORS = readFile lsColors;
-
-      # Nushell-only workaround:
       programs.nushell.extraConfig = "source ${
         pkgs.writeText "colors.nu" /* nu */ ''
           $env.LS_COLORS = open --raw ${lsColors}
