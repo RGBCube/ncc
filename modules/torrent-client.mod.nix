c4698ba658bf901ca81e17b4caabee1fefaf8c45
RGBCube <git@rgbcu.be>
2026-07-09 16:58:05 +0300

    qbittorrent: declare config

--- modules/torrent-client.mod.nix
+++ modules/torrent-client.mod.nix
@@ -10,6 +10,7 @@
       inherit (lib.modules) mkIf;
       inherit (lib.trivial) const flip;
       inherit (lib.attrsets) genAttrs;
+      inherit (lib.generators) toINI;
     in
     {
       xdg.mime-apps.default-applications =
@@ -22,5 +23,20 @@
       packages = [
         pkgs.qbittorrent
       ];
+
+      xdg.config.files."qBittorrent/qBittorrent.${
+        if osConfig.nixpkgs.hostPlatform.isDarwin then "ini" else "conf"
+      }" =
+        {
+          generator = toINI { };
+          value = {
+            Meta.MigrationVersion = 8;
+            LegalNotice.Accepted = true;
+
+            Preferences."Advanced\\updateCheck" = false;
+
+            BitTorrent."Session\\Port" = 7035;
+          };
+        };
     };
 }
