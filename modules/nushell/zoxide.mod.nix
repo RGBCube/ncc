ba17474ac1d31f8185f4e98780e868c8df0c611b
RGBCube <git@rgbcu.be>
2026-07-21 22:08:13 +0300

    zoxide: don't use hjem rum

--- modules/nushell/zoxide.mod.nix
+++ modules/nushell/zoxide.mod.nix
@@ -1,16 +1,20 @@
 { self, ... }:
 {
   flake.homeModules.shell = self.homeModules.zoxide;
-  flake.homeModules.zoxide = {
-    programs.zoxide = {
-      enable = true;
+  flake.homeModules.zoxide =
+    { lib, pkgs, ... }:
+    let
+      inherit (lib.meta) getExe;
+      inherit (lib.modules) mkAfter;
+      inherit (lib.lists) singleton;
+    in
+    {
+      packages = singleton pkgs.zoxide;
 
-      flags = [
-        "--cmd"
-        "cd"
-      ];
-
-      integrations.nushell.enable = true;
+      programs.nushell.extraConfig = mkAfter "source ${
+        pkgs.runCommand "zoxide.nu" { } /* bash */ ''
+          ${getExe pkgs.zoxide} init nushell --cmd cd > $out
+        ''
+      }";
     };
-  };
 }
