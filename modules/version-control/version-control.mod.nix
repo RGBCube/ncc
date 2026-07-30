58fec2871925f38f2094348997f2757b6e55c98c
RGBCube <git@rgbcu.be>
2026-07-30 23:30:34 +0300

    jujutsu: jj open

--- modules/version-control/version-control.mod.nix
+++ modules/version-control/version-control.mod.nix
@@ -167,6 +167,124 @@
 
         aliases.u = [ "undo" ];
 
+        aliases.open = [
+          "util"
+          "exec"
+          "--"
+          (pkgs.writers.writeNu "jj-open" /* nu */ ''
+            def resolve-revision [revision: string] {
+              do { jj --ignore-working-copy log --no-graph --template commit_id --revisions $revision }
+              | complete
+              | if $in.exit_code == 0 { $in.stdout }
+            }
+
+            def resolve-revision-in [--remote: any, symbol: string] {
+              [(if $remote != null { $"($symbol)@($remote)" }) $symbol]
+              | compact
+              | where {|candidate| resolve-revision $candidate | is-not-empty }
+              | get --optional 0
+            }
+
+            # Open a GitHub/Forgejo/etc URL locally, cloning the project to ~/Projects (by default) if it doesn't exist.
+            def main [
+              --at: path = ~/Projects # Directory the repository will be cloned into.
+              url: string # URL to the file to open.
+            ] {
+              let span = (metadata $url).span
+              let url = $url | url parse
+
+              # PARSE
+              let target = match ($url.path | split row "/" | where { is-not-empty }) {
+                [$owner, $repository] => {
+                  owner: $owner,
+                  repository: $repository,
+                  revision: "@",
+                  file: "."
+                }
+
+                # <owner>/<repository>/{blob,tree}/<revision>/<file>
+                [$owner, $repository, _, $revision, ..$file] => {
+                  owner: $owner
+                  repository: $repository
+                  revision: $'"($revision)"'
+                  file: ($file | path join | default "." --empty)
+                }
+
+                _ => {
+                  error make {
+                    msg: "not a repository url"
+                    label: {
+                      text: "expected <owner>/<repository>"
+                      span: $span
+                    }
+                  }
+                }
+              }
+              | insert line ($url.fragment | parse --regex 'L(?<line>\d+)' | get --optional line.0)
+
+              # CLONE
+              do --env {
+                let destination = $at | path join $target.repository
+
+                if not ($destination | path exists) {
+                  {
+                    scheme: $url.scheme
+                    host: $url.host
+                    path: $"/($target.owner)/($target.repository)"
+                  }
+                  | url join
+                  | jj git clone $in $destination
+                }
+
+                cd $destination
+              }
+
+              # RESOLVE
+              let symbol = do {
+                let remote = jj git remote list
+                | lines
+                | parse "{name} {url}"
+                | where {|remote| $remote.url | str replace --regex '\.git$' "" | str ends-with $"($target.owner)/($target.repository)" }
+                | get --optional name.0
+
+                let symbol = resolve-revision-in --remote $remote $target.revision
+
+                if $symbol != null {
+                  return $symbol
+                }
+
+                jj git fetch
+                let symbol = resolve-revision-in --remote $remote $target.revision
+
+                if $symbol == null {
+                  error make {
+                    msg: $"unknown revision: ($target.revision)"
+                    label: {
+                      text: "from here"
+                      span: $span
+                    }
+                  }
+                }
+
+                $symbol
+              }
+
+              # CHECKOUT
+              if (resolve-revision $"($symbol) & @ | ($symbol) & @-" | is-empty) {
+                # Create a fresh revision and make it the workspace
+                # if the revision to open is not current or the parent.
+                jj new $symbol
+              }
+
+              # EDIT
+              exec $env.EDITOR (match $target.line {
+                null => $target.file
+                $line => $"($target.file):($line)"
+              })
+            }
+          '')
+        ];
+
         aliases.fork = [
           "util"
           "exec"
